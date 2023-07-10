b3ea699528217c02b20c2f02df596f67c19b20b1
violator <violator@student.21-school.ru>
2023-07-10 17:47:59 +0300

    model

--- MVC/src/main/java/com/example/controllers/FirstController.java
+++ MVC/src/main/java/com/example/controllers/FirstController.java
@@ -3,6 +3,7 @@ package com.example.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,8 +23,10 @@ public class FirstController {
   @GetMapping("/goodbye")
   public String goodByePage(
       @RequestParam(value = "name", required = false) String name,
-      @RequestParam(value = "surname", required = false) String surname) {
-    System.out.format("goodbye    name = %s, surname = %s\n", name, surname);
+      @RequestParam(value = "surname", required = false) String surname,
+      Model model) {
+    model.addAttribute("message", String.format("goodbye    name = %s, surname = %s\n", name, surname));
+//    System.out.format("goodbye    name = %s, surname = %s\n", name, surname);
     return "first/goodbye";
   }
 }
