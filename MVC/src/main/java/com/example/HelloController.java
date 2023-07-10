87e27499504ede15125a8e361a5051f874c42845
violator <violator@student.21-school.ru>
2023-07-10 12:59:37 +0300

    web serv

---
+++ MVC/src/main/java/com/example/HelloController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/hello-world")
+  public String sayHello() {
+    return "hello_world";
+  }
+}
