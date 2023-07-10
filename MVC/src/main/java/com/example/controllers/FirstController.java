a0ff9c510217f7d14a54a352e2c93154574a2317
violator <violator@student.21-school.ru>
2023-07-10 14:02:07 +0300

    request mapping

--- MVC/src/main/java/com/example/controllers/FirstController.java
+++ MVC/src/main/java/com/example/controllers/FirstController.java
@@ -3,8 +3,10 @@ package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
   @GetMapping("/hello")
