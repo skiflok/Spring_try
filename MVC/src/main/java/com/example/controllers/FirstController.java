77bd3bd22b5b13234f4e88ccebba98911486b010
violator <violator@student.21-school.ru>
2023-07-10 17:10:34 +0300

    HttpServletRequest @RequestParam

--- MVC/src/main/java/com/example/controllers/FirstController.java
+++ MVC/src/main/java/com/example/controllers/FirstController.java
@@ -1,22 +1,29 @@
 package com.example.controllers;
 
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
   @GetMapping("/hello")
-  public String helloPage() {
+  public String helloPage(HttpServletRequest httpServletRequest) {
+    String name = httpServletRequest.getParameter("name");
+    String surname = httpServletRequest.getParameter("surname");
+    System.out.format("name = %s, surname = %s\n", name, surname);
     return "first/hello";
   }
 
   @GetMapping("/goodbye")
-  public String goodByePage() {
+  public String goodByePage(
+      @RequestParam(value = "name", required = false) String name,
+      @RequestParam(value = "surname", required = false) String surname) {
+    System.out.format("goodbye    name = %s, surname = %s\n", name, surname);
     return "first/goodbye";
   }
-
 }
