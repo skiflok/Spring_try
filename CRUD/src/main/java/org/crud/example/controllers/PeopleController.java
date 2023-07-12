4f0c4bbb38a3a63d63938b6ffd06fd22343166b2
violator <violator@student.21-school.ru>
2023-07-12 17:39:04 +0300

    validation

--- CRUD/src/main/java/org/crud/example/controllers/PeopleController.java
+++ CRUD/src/main/java/org/crud/example/controllers/PeopleController.java
@@ -1,11 +1,13 @@
 package org.crud.example.controllers;
 
 
+import javax.validation.Valid;
 import org.crud.example.dao.PersonDAO;
 import org.crud.example.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -43,7 +45,11 @@ public class PeopleController {
   }
 
   @PostMapping()
-  public String create(@ModelAttribute("person") Person person) {
+  public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+
+    if (bindingResult.hasErrors()) {
+      return "people/new";
+    }
     personDAO.save(person);
     return "redirect:/people";
   }
@@ -55,7 +61,10 @@ public class PeopleController {
 }
 
   @PatchMapping("/{id}")
-  public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+  public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult , @PathVariable("id") int id) {
+    if (bindingResult.hasErrors()) {
+      return "people/edit";
+    }
     personDAO.update(id, person);
     return "redirect:/people";
   }
--- CRUD/src/main/java/org/crud/example/models/Person.java
+++ CRUD/src/main/java/org/crud/example/models/Person.java
@@ -1,5 +1,9 @@
 package org.crud.example.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Person {
   private int id;
+  @NotEmpty(message = "name not be empty")
+  @Size(min = 2, max = 30, message = "name should be 2 and 30 character")
   private String name;
+  @Min(value = 0, message = "age should be greater than 0")
   private int age;
+  @NotEmpty(message = "Email should not be empty")
+  @Email(message = "Email should be valid")
   private String email;
 }
